e533354d8cc45fd9c25a0ed1f492ad0b07fce25b
Vogt-Kielland <fernando-flores.vogt-kielland@capgemini.com>
2024-03-26 10:49:06 +0100

    working with video and webcam. adding class filtering

--- app.py
+++ app.py
@@ -1,87 +1,153 @@
 from ultralytics import YOLO
+import torch
 import cv2
 import os
 import streamlit as st
+from classes import classes as model_classes
+import pandas as pd
 
 
 class WebApp:
     def __init__(self):
-        self.model = YOLO("yolov8n.pt")
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = self.load_model()
+        self.initialize_session_state()
         self.create_sidebar()
-        self.play_stored_video()
+        self.create_window()
+
+    def initialize_session_state(self):
+        if "clicked" not in st.session_state:
+            st.session_state.clicked = True
+
+    @st.cache_resource
+    def load_model(_self):
+        return YOLO("yolov8n.pt")
+
+    @st.cache_data
+    def load_classes(_self):
+        df = pd.DataFrame(
+            {
+                "classes": model_classes.values(),
+                "enabled": [False for c in model_classes],
+            }
+        )
+        return df
 
     def create_sidebar(self):
         with st.sidebar:
-            self.show_original_video = st.radio(
-                "Show original video", ["Yes", "No"], index=0, disabled=False
+            self.inference_source = st.radio(
+                "Inference Source", ["Video", "Webcam"], index=0, disabled=False
             )
             self.show_processed_video = st.radio(
-                "Show processed video", ["Yes", "No"], index=1, disabled=False
+                "Choose feed", ["Original", "Processed"], index=0, disabled=False
             )
 
+            self.confidence = st.slider("Desired confidence:", 0.1, 1.0, 0.2, 0.1)
+            self.iou = st.slider("Desired Intersection Over Union:", 0.1, 1.0, 0.7, 0.1)
+
     def file_selector(self, folder_path="test-videos/"):
-        file_names = os.listdir(folder_path)
-        selected_file = st.selectbox("choose a file", file_names)
-        return folder_path, selected_file
+        try:
+            file_names = os.listdir(folder_path)
+            selected_file = st.selectbox("choose a file", file_names)
+            return folder_path, selected_file
+        except:
+            st.sidebar.error("Directory not found")
 
-    def play_stored_video(self):
+    def create_class_selector(self):
+        with st.expander("Select objects to be detected"):
+            classes_df = self.load_classes()
 
-        folder_path, selected_file = self.file_selector()
+            edited_df = st.data_editor(
+                classes_df,
+                column_config={
+                    "enabled": st.column_config.CheckboxColumn(
+                        "Enabled",
+                        help="Select the classes you want the model to detect",
+                    )
+                },
+                disabled=["Classes"],
+                hide_index=True,
+            )
 
-        video_path = f"{folder_path}{selected_file}"
-
-        if self.show_original_video == "Yes":
-            st.markdown("# Original Video")
-            with open(video_path, "rb") as video_file:
-                video_bytes = video_file.read()
-            if video_bytes:
-                st.video(video_bytes)
-        if self.show_processed_video == "Yes":
-            st.markdown("# Processed Video")
-            try:
-                vid_cap = cv2.VideoCapture(str(video_path))
-                st_frame = st.empty()
-                while vid_cap.isOpened():
-                    success, image = vid_cap.read()
-                    if success:
-                        self.display_detected_frames(
-                            self.model,
-                            st_frame,
-                            image,
-                        )
-                    else:
-                        vid_cap.release()
-                        break
-            except Exception as e:
-                st.sidebar.error("Error loading video: " + str(e))
+            filtered_df = edited_df[edited_df["enabled"] == True]
+            self.selected_classes = list(filtered_df.index.values)
 
     def display_detected_frames(
         self,
         model,
         st_frame,
         image,
-        conf: float = 0.6,
     ):
-        """
-        Display the detected objects on a video frame using the YOLOv8 model.
-
-        Args:
-        - model (YoloV8): A YOLOv8 object detection model.
-        - st_frame (Streamlit object): A Streamlit object to display the detected video.
-        - image (numpy array): A numpy array representing the video frame.
-        - conf (float): Confidence threshold for object detection.
-        """
 
         # Resize the image to a standard size
         image = cv2.resize(image, (720, int(720 * (9 / 16))))
 
-        res = model.predict(image, conf=conf)
+        y_pred = model.predict(
+            image,
+            conf=self.confidence,
+            stream_buffer=True,
+            iou=self.iou,
+            device=self.device,
+            classes=self.selected_classes,
+        )
 
         # # Plot the detected objects on the video frame
-        res_plotted = res[0].plot()
+        boxes_plotted = y_pred[0].plot()
         st_frame.image(
-            res_plotted, caption="Detected Video", channels="BGR", use_column_width=True
+            boxes_plotted,
+            caption="Detected Video",
+            channels="BGR",
+            use_column_width=True,
         )
+
+    def create_window(self):
+
+        st.title("Object Detection Prototype")
+        self.create_class_selector()
+
+        try:
+
+            folder_path, selected_file = self.file_selector()
+
+            video_path = f"{folder_path}{selected_file}"
+
+            if self.show_processed_video != "Processed":
+                st.markdown("# Original Video")
+
+                with open(video_path, "rb") as video_file:
+                    video_bytes = video_file.read()
+                if video_bytes:
+                    st.video(video_bytes)
+
+            if self.show_processed_video == "Processed":
+                st.markdown("# Processed Video")
+
+                try:
+                    if self.inference_source == "Webcam":
+                        video_feed = cv2.VideoCapture(0)
+                    else:
+                        video_feed = cv2.VideoCapture(str(video_path))
+
+                    st_frame = st.empty()
+
+                    while video_feed.isOpened():
+                        success, image = video_feed.read()
+
+                        if success:
+                            self.display_detected_frames(
+                                self.model,
+                                st_frame,
+                                image,
+                            )
+                        else:
+                            video_feed.release()
+                            break
+
+                except Exception as e:
+                    st.sidebar.error("Error loading video: " + str(e))
+
+        except TypeError:
+            st.markdown("we couldn't find")
 
 
 if __name__ == "__main__":
